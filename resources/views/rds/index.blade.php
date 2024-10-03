af5703bf93dcbb7207c677450fd7311ae9afe255
Goldfroy <houegbelossiallode@gmail.com>
2024-10-03 10:34:56 +0100

    add code commercial

--- resources/views/rds/index.blade.php
+++ resources/views/rds/index.blade.php
@@ -57,7 +57,7 @@
                                         <td>{{ $rd->notes}}</td>
                                         <td>{{ $rd->prime}}</td>
                                         <td>{{ \Carbon\Carbon::parse($rd->date_du_rdv)->format('d/m/Y')}}</td>
-                                        <td>{{ $rd->commercial->user->nom ?? 'Commercial non défini'}}</td>
+                                        <td>{{ $rd->commercial->user->nom }}</td>
                                         <td>
 
                                                 <a href="{{ route('rds.edit', $rd->id) }}"
