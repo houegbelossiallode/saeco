8548054dcc6e34ec2b55919eaeca8ccf197a78f9
Goldfroy <houegbelossiallode@gmail.com>
2024-10-04 10:34:18 +0100

    add code commercial

--- app/Models/Rd.php
+++ app/Models/Rd.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Rd extends Model
 {
-    use HasFactory;
+    use HasFactory,Notifiable;
     protected $fillable = ['prime','produit_id','client_id','commercial_id','notes','date_du_rdv'];
 
     public function client()
